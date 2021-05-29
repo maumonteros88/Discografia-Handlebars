7685ec9c7ad1f23e68d068be4226a8dd7dfcb624
Mauricio <mau.monteros88@gmail.com>
2021-05-29 12:45:45 -0300

    initial commit

---
+++ back/server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const exphas = require("express-handlebars");
+const path = require("path");
+const app = express();
+const discos = require("./discos.json");
+
+const PORT = 3034;
+const index = path.join(__dirname, "../public", "index.html");
+
+app.engine(
+  "handlebars",
+  exphas({
+    defaultLayout: "main-layout",
+    layoutsDir: "views/layouts",
+  })
+);
+
+console.log(path.join(__dirname, "../views/layouts"));
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "../views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.render("home",{titulo:"Welcome"})
+});
+
+app.get("/buscar", (req, res) => {
+  let titles = req.query.titles;
+  let artist = req.query.artist;
+  let launch = req.query.launch;
+  let result = discos.discos;
+
+  if (artist) {
+    result = result.filter((element) =>
+      element.artista
+        .toLocaleLowerCase()
+        .includes(artist.toString().toLocaleLowerCase())
+    );
+  }
+
+  if (titles) {
+    result = result.filter((element) =>
+      element.titulo
+        .toLocaleLowerCase()
+        .includes(titles.toString().toLocaleLowerCase())
+    );
+  }
+
+  if (launch) {
+    result = result.filter(
+      (element) => element.lanzamiento.toString() === launch
+    );
+  }
+
+  if (artist && launch) {
+    result = result.filter(
+      (element) =>
+        element.lanzamiento.toString() === launch &&
+        element.artista
+          .toLocaleLowerCase()
+          .includes(artist.toString().toLocaleLowerCase())
+    );
+  }
+
+  if (titles && launch) {
+    result = result.filter(
+      (element) =>
+        element.lanzamiento.toString() === launch &&
+        element.titulo
+          .toLocaleLowerCase()
+          .includes(titles.toString().toLocaleLowerCase())
+    );
+  }
+
+  if (titles && launch && artist) {
+    result = result.filter(
+      (element) =>
+        element.lanzamiento.toString() === launch &&
+        element.titulo
+          .toLocaleLowerCase()
+          .includes(titles.toString().toLocaleLowerCase()) &&
+        element.artista
+          .toLocaleLowerCase()
+          .includes(artist.toString().toLocaleLowerCase())
+    );
+  }
+
+  res.render("cards", {
+    discos: result,
+  });
+});
+
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  console.log("Server is up and listening on PORT:", PORT);
+});
---
+++ public/scritp.js
@@ -0,0 +1,46 @@
+let cards = document.getElementById("cards");
+const search = document.getElementById("search");
+const loading = document.getElementById("loading")
+
+const xhr = new XMLHttpRequest();
+
+search.addEventListener("click", () => {
+  loading.style.display="block"
+  // @ts-ignore
+  const artist = document.getElementById("artist").value;
+  // @ts-ignore
+  const titles = document.getElementById("titles").value;
+  // @ts-ignore
+  const launch = document.getElementById("launch").value;
+
+  xhr.addEventListener("load", () => {
+    cards.innerHTML = "";
+    const response = JSON.parse(xhr.responseText);
+
+    response.forEach((element) => {
+      cards.innerHTML += 
+       `<div class="card col-4" style="width: 18rem; margin-left:10px; margin-top: 10px">
+        <img src="${element.tapa}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${element.artista}</h5>
+        <h6 class="card-title">${element.titulo}</h6>
+        <p class="card-text">Lanzamiento: ${element.lanzamiento}</p>        
+        </div>
+        </div>`;
+    });
+  });
+
+  let filter = "";
+
+  if (artist) {
+    filter += (filter ? "&" : "") + `artist=${artist}`;
+  }
+  if (titles) {
+    filter += (filter ? "&" : "") + `titles=${titles}`;
+  }
+  if (launch) {
+    filter += (filter ? "&" : "") + `launch=${launch}`;
+  }
+  xhr.open("GET", `/discos?${filter}`);
+  xhr.send();
+});
